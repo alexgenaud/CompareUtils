126ccfc6094f5f558a1254eeb09172f82416d398
Alexander E Genaud <alex@genaud.net>
2023-03-27 16:14:49 +0200

    More BigDecimal equality tests

--- app/src/test/java/net/genaud/compareutils/BigDecimalZeroTest.java
+++ app/src/test/java/net/genaud/compareutils/BigDecimalZeroTest.java
@@ -3,72 +3,84 @@ package net.genaud.compareutils;
 import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertLinesMatch;
-import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BigDecimalZeroTest {
+    final double dZero = 0.0;
+
     @Test void zero_equals_zero() {
         assertTrue(BigDecimal.ZERO.equals(BigDecimal.ZERO));
-
         assertTrue(BigDecimal.ZERO.equals(new BigDecimal(0)));
-        assertTrue(BigDecimal.ZERO.equals(new BigDecimal(0.0)));
+        assertTrue(BigDecimal.ZERO.equals(new BigDecimal(dZero)));
         assertTrue(BigDecimal.ZERO.equals(new BigDecimal("0")));
+        assertTrue(BigDecimal.ZERO.equals(BigDecimal.valueOf(0)));
+
+        assertTrue(BigDecimal.valueOf(0.00).equals(new BigDecimal("0.0")));
+    }
+
+    @Test void zero_not_equals_zero() {
+        assertFalse(BigDecimal.ZERO.equals(null));
+        assertFalse(BigDecimal.ONE.equals(null));
+
+        assertFalse(BigDecimal.ZERO.equals(Boolean.TRUE), "zero equals boolean true");
+        assertFalse(BigDecimal.ZERO.equals(Boolean.FALSE), "zero equals boolean false");
+
         assertFalse(BigDecimal.ZERO.equals(new BigDecimal("0.0")));
+        assertFalse(BigDecimal.ZERO.equals(BigDecimal.valueOf(0.0)));
 
-        assertTrue((new BigDecimal(0)).equals(BigDecimal.ZERO));
-        assertTrue((new BigDecimal(0.0)).equals(BigDecimal.ZERO));
-        assertTrue((new BigDecimal("0")).equals(BigDecimal.ZERO));
-        assertFalse((new BigDecimal("0.0")).equals(BigDecimal.ZERO));
-
-        assertTrue((new BigDecimal("0.0")).equals(new BigDecimal("0.0")));
-        assertTrue((new BigDecimal("0")).equals(new BigDecimal("0")));
-        assertFalse((new BigDecimal("0.0")).equals(new BigDecimal("0")));
-        assertFalse((new BigDecimal("0")).equals(new BigDecimal("0.0")));
-
-        // strange
-        assertFalse((new BigDecimal("0.0")).equals(new BigDecimal(0.0)));
-
-        assertTrue((new BigDecimal("0")).equals(new BigDecimal(0)));
-        assertFalse((new BigDecimal("0.0")).equals(new BigDecimal(0)));
-        assertTrue((new BigDecimal("0")).equals(new BigDecimal(0.0)));
-
-        assertFalse((new BigDecimal(0.0)).equals(new BigDecimal("0.0")));
-        assertTrue((new BigDecimal(0)).equals(new BigDecimal("0")));
-        assertTrue((new BigDecimal(0.0)).equals(new BigDecimal("0")));
         assertFalse((new BigDecimal(0)).equals(new BigDecimal("0.0")));
+        assertFalse((new BigDecimal(dZero)).equals(new BigDecimal("0.0")));
+        assertFalse((new BigDecimal("0")).equals(new BigDecimal("0.0")));
+        assertFalse(BigDecimal.valueOf(0).equals(new BigDecimal("0.0")));
+
+        assertFalse((new BigDecimal("0.0")).equals(BigDecimal.ZERO));
+        assertFalse((new BigDecimal("0.0")).equals(new BigDecimal(0)));
+        assertFalse((new BigDecimal("0.0")).equals(new BigDecimal(dZero)));
+        assertFalse((new BigDecimal("0.0")).equals(new BigDecimal("0")));
+        assertFalse((new BigDecimal("0.0")).equals(new BigDecimal("0.00")));
+        assertFalse((new BigDecimal("0.0")).equals(BigDecimal.valueOf(0)));
+
+        assertFalse(BigDecimal.valueOf(0.0).equals(new BigDecimal("0")));
+        assertFalse(BigDecimal.valueOf(0).equals(new BigDecimal("0.0")));
+        assertFalse(BigDecimal.valueOf(0.0).equals(new BigDecimal("0.00")));
     }
 
     @Test void zero_compareto_zero() {
-        assertEquals(BigDecimal.ZERO.compareTo(BigDecimal.ZERO), 0);
+//        System.out.println(BigDecimal.ZERO.compareTo(null));
+        assertThrows(NullPointerException.class, () -> BigDecimal.ZERO.compareTo(null));
+        assertThrows(NullPointerException.class, () -> BigDecimal.ONE.compareTo(null));
 
+        assertEquals(BigDecimal.ZERO.compareTo(BigDecimal.ZERO), 0);
         assertEquals(BigDecimal.ZERO.compareTo(new BigDecimal(0)), 0);
-        assertEquals(BigDecimal.ZERO.compareTo(new BigDecimal(0.0)), 0);
+        assertEquals(BigDecimal.ZERO.compareTo(BigDecimal.valueOf(0.0)), 0);
         assertEquals(BigDecimal.ZERO.compareTo(new BigDecimal("0")), 0);
         assertEquals(BigDecimal.ZERO.compareTo(new BigDecimal("0.0")), 0);
 
         assertEquals((new BigDecimal(0)).compareTo(BigDecimal.ZERO), 0);
-        assertEquals((new BigDecimal(0.0)).compareTo(BigDecimal.ZERO), 0);
-        assertEquals((new BigDecimal("0")).compareTo(BigDecimal.ZERO), 0);
-        assertEquals((new BigDecimal("0.0")).compareTo(BigDecimal.ZERO), 0);
+        assertEquals((new BigDecimal(0)).compareTo(new BigDecimal(0)), 0);
+        assertEquals((new BigDecimal(0)).compareTo(BigDecimal.valueOf(0.0)), 0);
+        assertEquals((new BigDecimal(0)).compareTo(new BigDecimal("0")), 0);
+        assertEquals((new BigDecimal(0)).compareTo(new BigDecimal("0.0")), 0);
 
-        assertEquals((new BigDecimal("0.0")).compareTo(new BigDecimal("0.0")), 0);
+        assertEquals(BigDecimal.valueOf(0.0).compareTo(BigDecimal.ZERO), 0);
+        assertEquals(BigDecimal.valueOf(0.0).compareTo(new BigDecimal(0)), 0);
+        assertEquals(BigDecimal.valueOf(0.0).compareTo(BigDecimal.valueOf(0.0)), 0);
+        assertEquals(BigDecimal.valueOf(0.0).compareTo(new BigDecimal("0")), 0);
+        assertEquals(BigDecimal.valueOf(0.0).compareTo(new BigDecimal("0.0")), 0);
+
+        assertEquals((new BigDecimal("0")).compareTo(BigDecimal.ZERO), 0);
+        assertEquals((new BigDecimal("0")).compareTo(new BigDecimal(0)), 0);
+        assertEquals((new BigDecimal("0")).compareTo(BigDecimal.valueOf(0.0)), 0);
         assertEquals((new BigDecimal("0")).compareTo(new BigDecimal("0")), 0);
-        assertEquals((new BigDecimal("0.0")).compareTo(new BigDecimal("0")), 0);
         assertEquals((new BigDecimal("0")).compareTo(new BigDecimal("0.0")), 0);
 
-        // strange
-        assertEquals((new BigDecimal("0.0")).compareTo(new BigDecimal(0.0)), 0);
-
-        assertEquals((new BigDecimal("0")).compareTo(new BigDecimal(0)), 0);
+        assertEquals((new BigDecimal("0.0")).compareTo(BigDecimal.ZERO), 0);
         assertEquals((new BigDecimal("0.0")).compareTo(new BigDecimal(0)), 0);
-        assertEquals((new BigDecimal("0")).compareTo(new BigDecimal(0.0)), 0);
-
-        assertEquals((new BigDecimal(0.0)).compareTo(new BigDecimal("0.0")), 0);
-        assertEquals((new BigDecimal(0)).compareTo(new BigDecimal("0")), 0);
-        assertEquals((new BigDecimal(0.0)).compareTo(new BigDecimal("0")), 0);
-        assertEquals((new BigDecimal(0)).compareTo(new BigDecimal("0.0")), 0);
+        assertEquals((new BigDecimal("0.0")).compareTo(BigDecimal.valueOf(0.0)), 0);
+        assertEquals((new BigDecimal("0.0")).compareTo(new BigDecimal("0")), 0);
+        assertEquals((new BigDecimal("0.0")).compareTo(new BigDecimal("0.0")), 0);
     }
 
 }
